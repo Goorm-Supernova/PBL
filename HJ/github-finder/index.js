51e085e3084b3c7fabfd8e6ef4f5fc329289e41b
Lee <94841127+galaxian@users.noreply.github.com>
2023-12-27 10:40:56 +0900

    feat: 검색창에 입력한 github 유저명의 프로필을 가져오는 기능 구현

---
+++ HJ/github-finder/index.js
@@ -0,0 +1,29 @@
+const github = new Github();
+
+const searchUser = document.querySelector(".search .search__input");
+
+searchUser.addEventListener("keyup", (e) => {
+    const user = e.target.value;
+    const userData = github.getUser(user).then((data) => {
+        document.getElementById("profiles").innerHTML = `<div class="profile">
+        <div class="row">
+            <div class="user__left-box">
+                <img class="user__image" src="${data.avatar_url}"/>
+                <a class="user__view-profile" href="${data.html_url}">View Profile </a>
+            </div>
+            <div class="user__right-box">
+                <span class="user__public-repo">Public Repo: ${data.public_repos}</span>
+                <span class="user__public-gists">Public Gists: ${data.public_gists}</span>
+                <span class="user__follower">Followers: ${data.followers}</span>
+                <span class="user__following">Following: ${data.following}</span>
+                <div class="user__info">
+                    <div>Company: ${data.company}</div>
+                    <div>Website/Blog: ${data.blog}</div>
+                    <div>Location: ${data.location}</div>
+                    <div>Member Since: ${data.created_at}</div>
+                </div>
+            </div>
+        </div>
+    </div>`;
+    });
+});
