9a02a3c4078edf0b46bd8d6e5856eca0a7efb2ae
Lee <94841127+galaxian@users.noreply.github.com>
2023-12-27 16:34:55 +0900

    feat: 화면 출력 부분을 View.js에서 처리하도록 구조 변경

---
+++ HJ/github-finder/View.js
@@ -0,0 +1,53 @@
+class View {
+    constructor() {
+        this.profile = document.getElementById("profiles");
+        this.repo = document.getElementById("repos");
+    }
+
+    viewProfile(data) {
+        this.profile.innerHTML = `<div class="profile">
+    <div class="profile__row">
+        <div class="user__left-box">
+            <img class="user__image" src="${data.avatar_url}"/>
+            <a class="user__view-profile" href="${data.html_url}">View Profile </a>
+        </div>
+        <div class="user__right-box">
+            <span class="user__public-repo">Public Repo: ${data.public_repos}</span>
+            <span class="user__public-gists">Public Gists: ${data.public_gists}</span>
+            <span class="user__follower">Followers: ${data.followers}</span>
+            <span class="user__following">Following: ${data.following}</span>
+            <div class="user__info">
+                <div>Company: ${data.company}</div>
+                <div>Website/Blog: ${data.blog}</div>
+                <div>Location: ${data.location}</div>
+                <div>Member Since: ${data.created_at}</div>
+            </div>
+        </div>
+    </div>
+</div>`;
+    }
+
+    viewRepo(repos) {
+        let addHTML = `<h2 class="repo-section__title">Latest Repos</h2>`;
+        for (const repo of repos) {
+            addHTML += `<div class="repo">
+    <div class="repo__row">
+        <div>
+            <a href="${repo.html_url}">${repo.name}</a>
+        </div>
+
+        <div>
+            <span class="repo__star">Stars: ${repo.stargazers_count}</span>
+            <span class="repo__watcher">Watchers: ${repo.watchers_count}</span>
+            <span class="repo__fork">Forks: ${repo.forms_count}</span>
+        </div>
+    </div>
+</div>`;
+        }
+        this.repo.innerHTML = addHTML;
+    }
+
+    viewNotFound() {
+        this.profile.innerHTML = `<h1>Not Found</h1>`;
+    }
+}
--- HJ/github-finder/index.js
+++ HJ/github-finder/index.js
@@ -1,4 +1,5 @@
 const github = new Github();
+const view = new View();
 
 const searchUser = document.querySelector(".search .search__input");
 
@@ -7,52 +8,13 @@ searchUser.addEventListener("keyup", (e) => {
     if (user !== "") {
         const userData = github.getUser(user).then((data) => {
             if (data.message === "Not Found") {
-                document.getElementById(
-                    "profiles"
-                ).innerHTML = `<h1>Not Found</h1>`;
+                view.viewNotFound();
             } else {
-                console.log("result", data);
-                document.getElementById(
-                    "profiles"
-                ).innerHTML = `<div class="profile">
-            <div class="profile__row">
-                <div class="user__left-box">
-                    <img class="user__image" src="${data.avatar_url}"/>
-                    <a class="user__view-profile" href="${data.html_url}">View Profile </a>
-                </div>
-                <div class="user__right-box">
-                    <span class="user__public-repo">Public Repo: ${data.public_repos}</span>
-                    <span class="user__public-gists">Public Gists: ${data.public_gists}</span>
-                    <span class="user__follower">Followers: ${data.followers}</span>
-                    <span class="user__following">Following: ${data.following}</span>
-                    <div class="user__info">
-                        <div>Company: ${data.company}</div>
-                        <div>Website/Blog: ${data.blog}</div>
-                        <div>Location: ${data.location}</div>
-                        <div>Member Since: ${data.created_at}</div>
-                    </div>
-                </div>
-            </div>
-        </div>`;
-                let addHTML = "";
+                // console.log("result", data);
+                view.viewProfile(data);
                 github.getRepo(user).then((repos) => {
-                    console.log("repo", repos);
-                    for (const repo of repos) {
-                        addHTML += `<div class="repo">
-                <div class="repo__row">
-                    <div>
-                        <a href="${repo.html_url}">${repo.name}</a>
-                    </div>
-    
-                    <div>
-                        <span class="repo__star">Stars: ${repo.stargazers_count}</span>
-                        <span class="repo__watcher">Watchers: ${repo.watchers_count}</span>
-                        <span class="repo__fork">Forks: ${repo.forms_count}</span>
-                    </div>
-                </div>
-            </div>`;
-                    }
-                    document.getElementById("repos").innerHTML = addHTML;
+                    // console.log("repo", repos);
+                    view.viewRepo(repos);
                 });
             }
         });
