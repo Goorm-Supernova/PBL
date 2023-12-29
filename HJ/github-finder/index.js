44bd60e607ddecbfff58695f6b99af4f51c86e03
Lee <94841127+galaxian@users.noreply.github.com>
2023-12-29 14:01:54 +0900

    feat: 검색창에서 enter 키 입력시 github 정보를 가져오도록 기능 수정

--- HJ/github-finder/index.js
+++ HJ/github-finder/index.js
@@ -4,22 +4,26 @@ const view = new View();
 const searchUser = document.querySelector(".search .search__input");
 
 searchUser.addEventListener("keyup", (e) => {
-    const user = e.target.value;
-    if (user !== "") {
-        const userData = github.getUser(user).then((data) => {
-            if (data.message === "Not Found") {
-                view.viewNotFound();
-            } else {
-                // console.log("result", data);
-                view.viewProfile(data);
-                view.viewCommitChart(user);
-                github.getRepo(user).then((repos) => {
-                    // console.log("repo", repos);
-                    view.viewRepo(repos);
-                });
-            }
-        });
-    } else {
-        document.getElementById("profiles").innerHTML = ``;
+    if (e.keyCode === 13) {
+        e.preventDefault();
+        const user = e.target.value;
+        if (user !== "") {
+            const userData = github.getUser(user).then((data) => {
+                if (data.message === "Not Found") {
+                    view.viewNotFound();
+                } else {
+                    // console.log("result", data);
+                    view.viewProfile(data);
+                    view.viewCommitChart(user);
+
+                    github.getRepo(user).then((repos) => {
+                        // console.log("repo", repos);
+                        view.viewRepo(repos);
+                    });
+                }
+            });
+        } else {
+            document.getElementById("profiles").innerHTML = ``;
+        }
     }
 });
