a55818c5dde04e33a122c3aebb52ea7b92ffd97d
Lee <94841127+galaxian@users.noreply.github.com>
2024-01-02 14:30:34 +0900

    feat: Cell element 생성 메서드 구현

--- HJ/spreadsheet/cell.js
+++ HJ/spreadsheet/cell.js
@@ -7,4 +7,13 @@ class Cell {
         this.column = column;
         this.active = active;
     }
+
+    createCellEl() {
+        const cellEl = document.createElement("input");
+        cellEl.className = "cell";
+        cellEl.id = "cell_" + this.row + this.column;
+        cellEl.value = this.data;
+        cellEl.disabled = this.disabled;
+        return cellEl;
+    }
 }
