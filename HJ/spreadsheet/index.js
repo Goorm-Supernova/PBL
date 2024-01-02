17a829faf2b1c9bb395aaaab8546e1831a91a716
Lee <94841127+galaxian@users.noreply.github.com>
2024-01-02 17:55:17 +0900

    feat: export 버튼 클릭 시 csv 파일로 변환 후 저장하는 로직 구현

--- HJ/spreadsheet/index.js
+++ HJ/spreadsheet/index.js
@@ -1,3 +1,33 @@
 const spreadSheetContainer = document.querySelector("#spreadsheet-container");
+const exportBtn = document.querySelector("#export-btn");
 
 const spreadSheet = new SpreadSheet(10, 10, spreadSheetContainer);
+
+exportBtn.onclick = function (e) {
+    const csv = createCSV(spreadSheet);
+    outputCSV(csv);
+};
+
+function createCSV(spreadSheet) {
+    let csv = "";
+    for (let i = 0; i < spreadSheet.spreadSheet.length; i++) {
+        if (i === 0) continue;
+        csv +=
+            spreadSheet.spreadSheet[i]
+                .filter((item) => !item.isHeader)
+                .map((item) => item.data)
+                .join(",") + "\r\n";
+    }
+
+    return csv;
+}
+
+function outputCSV(csv) {
+    const csvObj = new Blob([csv]);
+    const csvUrl = URL.createObjectURL(csvObj);
+
+    const a = document.createElement("a");
+    a.href = csvUrl;
+    a.download = "spreadsheet name.csv";
+    a.click();
+}
