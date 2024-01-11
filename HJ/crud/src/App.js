b2784fed6261c7d647c913b13f5433b06d75b27c
Lee <94841127+galaxian@users.noreply.github.com>
2024-01-11 17:40:39 +0900

    feat: 예산 정보 입력 후 클릭 시 예산 목록에 조회하는 기능 구현

--- HJ/crud/src/App.js
+++ HJ/crud/src/App.js
@@ -1,25 +1,30 @@
-import { useState } from "react";
+import { useCallback, useRef, useState } from "react";
 import "./App.css";
 import BudgetContainer from "./components/BudgetContainer.js";
 import BudgetInput from "./components/BudgetInput.js";
 import BudgetList from "./components/BudgetList.js";
 
 function App() {
-  const [budgets, setBudgets] = useState([
-    {
-      id: 1,
-      title: "식비",
-      price: 1000,
+  const [budgets, setBudgets] = useState([]);
+
+  const id = useRef(1);
+
+  const onInsert = useCallback(
+    (title, price) => {
+      const budget = {
+        id: id.current,
+        title,
+        price,
+      };
+      setBudgets(budgets.concat(budget));
+      id.current += 1;
     },
-    {
-      id: 2,
-      title: "숙박비",
-      price: 10000,
-    },
-  ]);
+    [budgets]
+  );
+
   return (
     <BudgetContainer>
-      <BudgetInput />
+      <BudgetInput onInsert={onInsert} />
       <BudgetList budgetItems={budgets} />
     </BudgetContainer>
   );
--- HJ/crud/src/components/BudgetInput.js
+++ HJ/crud/src/components/BudgetInput.js
@@ -1,21 +1,51 @@
-import React from "react";
+import React, { useCallback, useState } from "react";
 import "./BudgetInput.css";
 
-function BudgetInput() {
+function BudgetInput({ onInsert }) {
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+
+  const onChangeTitle = useCallback((e) => {
+    setTitle(e.target.value);
+  }, []);
+
+  const onChangePrice = useCallback((e) => {
+    setPrice(e.target.value);
+  }, []);
+
+  const onClick = useCallback(
+    (e) => {
+      onInsert(title, price);
+      setTitle("");
+      setPrice("");
+    },
+    [onInsert, title, price]
+  );
+
   return (
     <div className="input-container">
       <div className="budget-input">
         <div>
           <p>지출 항목</p>
-          <input placeholder="예) 식비" />
+          <input
+            placeholder="예) 식비"
+            value={title}
+            onChange={onChangeTitle}
+          />
         </div>
 
         <div>
           <p>비용</p>
-          <input placeholder="예) 1200" />
+          <input
+            placeholder="예) 1200"
+            value={price}
+            onChange={onChangePrice}
+          />
         </div>
       </div>
-      <button className="btn">제출</button>
+      <button className="btn" onClick={onClick}>
+        제출
+      </button>
     </div>
   );
 }
