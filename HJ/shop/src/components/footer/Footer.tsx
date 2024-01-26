5188d83e2e43e53d06a8388229c8269f73fb578d
Lee <94841127+galaxian@users.noreply.github.com>
2024-01-26 12:02:17 +0900

    feat: header layout 구현

---
+++ HJ/shop/src/components/footer/Footer.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Footer.module.scss";
+
+const Footer = () => {
+  return (
+    <footer className={styles.footer}>
+      <div className="container">
+        <div className={styles.contacts}>
+          <a href="https://github.com/"> {/* <BsGithub/> */}</a>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ HJ/shop/src/components/layout/Layout.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from "./Layout.module.scss";
+import Header from "../header/Header";
+import Footer from "../footer/Footer";
+
+const Layout = () => {
+  return (
+    <div className={styles.layout}>
+      <Header />
+      {/* <Outlet/> */}
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
